3d7b29bab198857d91b0932fe51c24cec105fd7b
Mujtaba Asim <mujtabaasim701@gmail.com>
2024-08-05 15:59:15 +0100

    made a calculator in python

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 print(" Hi my name is Mujtaba Asim this is my new practice program")
 print(17*45)
-a = 49
-b = 79
+a1 = 49
+b2 = 79
 c = True
 d = None
 e = 2.2
-f = complex(8,2)
+f = complex(8, 2)
 print("how to go on a different line without \n"
       "using a new string ")
-print("the type of a is ", type(a))
-print("the type of b is ", type(b))
+print("the type of a is ", type(a1))
+print("the type of b is ", type(b2))
 print("the type of c is ", type(c))
 print("the type of d is ", type(d))
 print("the type of e is ", type(e))
@@ -20,10 +20,17 @@ list1 = [2, 3.1, [-5, 4], ["apple", "banana"]]
 print(list1)
 tupple1 = ("tiger", "lion", ("fly", "Ant"))
 print(tupple1)
-print(a*b)
+print(a1*b2)
 print(5+6)
 print(8-2)
 print(9*8)
 print(9/2)
 print(9//2)
 print(5**5)
+a = int(input("Enter the value of a = "))
+b = int(input("Enter the value of b = "))
+print("a+b =", a+b)
+print("a-b =", a-b)
+print("a*b =", a*b)
+print("a/b =", a/b)
+print("a Percentile b", a % b)
