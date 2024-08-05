4f176273344ed8f8afd2a0400b7b6fe88108a531
Mujtaba Asim <mujtabaasim701@gmail.com>
2024-08-05 13:20:49 +0100

    practice

---
+++ main.py
@@ -0,0 +1,29 @@
+print(" Hi my name is Mujtaba Asim this is my new practice program")
+print(17*45)
+a = 49
+b = 79
+c = True
+d = None
+e = 2.2
+f = complex(8,2)
+print("how to go on a different line without \n"
+      "using a new string ")
+print("the type of a is ", type(a))
+print("the type of b is ", type(b))
+print("the type of c is ", type(c))
+print("the type of d is ", type(d))
+print("the type of e is ", type(e))
+print("the type of f is ", type(f))
+dict1 = {"name": "Mujtaba", "age": 20, "can vote": True}
+print(dict1)
+list1 = [2, 3.1, [-5, 4], ["apple", "banana"]]
+print(list1)
+tupple1 = ("tiger", "lion", ("fly", "Ant"))
+print(tupple1)
+print(a*b)
+print(5+6)
+print(8-2)
+print(9*8)
+print(9/2)
+print(9//2)
+print(5**5)
