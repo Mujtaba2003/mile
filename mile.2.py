f63f6e3fc9b80d884ba44dbaa0f5045ee804f3e1
Mujtaba Asim <mujtabaasim701@gmail.com>
2024-08-05 17:00:25 +0100

    paractice

---
+++ mile.2.py
@@ -0,0 +1,14 @@
+# a = input("enter input ")
+# print(a)
+# b = input("enter input ")
+# print(b)
+# print(a+b)
+# c = int(input("enter input "))
+# print(c)
+# d = int(input("enter input "))
+# print(d)
+# print(c+d)
+
+
+e= bool (input("enter anything"))
+print(e)
