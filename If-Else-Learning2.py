df09fa5b373cbc13fcda6a06f34c3c2631275e3b
Mujtaba Asim <mujtabaasim701@gmail.com>
2024-08-10 14:57:48 +0100

    practice

--- If-Else-Learning2.py
+++ If-Else-Learning2.py
@@ -13,14 +13,56 @@
 #check kere Mujtaba print kerde mujtaba found
 #kuch bhi nahi ha print i dont know
 
-#My name is Kashan Asim
-#My name is Mujtaba Asim
-#My name is Mujtaba Asim and my brother's name is Is Kashan Asim
+#My name is kashan asim
+#My name is mujtaba asim
+#My name is mujtaba asim and my brother's name is Is kashan asim
 
-o = input("Enter a name")
-if o. __contains__("kashan"):
-    print("kashan is found")
-elif o. __contains__("mujtaba"):
-    print(" mujtaba is found")
-else:
-    print("kuch bhi nai ha")
+# o = input("Enter a name")
+# if o. __contains__("kashan"):
+#     print("kashan is found")
+# elif o. __contains__("mujtaba"):
+#     print(" mujtaba is found")
+# else:
+#     print("kuch bhi nai ha")
+
+
+
+
+
+# what = '''My name is kashan asim
+# My name is mujtaba asim
+# My name is mujtaba asim and my brother's name is Is kashan asim
+# and i m trying to print a single string consisting of multiple'''
+# print(what)
+#
+# st = """check kere Kashan print kerde kashan found
+# #check kere Mujtaba print kerde mujtaba found
+# #kuch bhi nahi ha print i dont know
+# #
+# # #My name is kashan asim
+# # #My name is mujtaba asim
+# # #My name is mujtaba asim and my brother's name is kashan asim"""
+# # print(st)
+#
+# name = "harry"
+# friend = "humaim"
+# print(name,friend)
+# otherfriend = 'my name is husnain " and i m studing" in third semester'
+# print( "harry"[0])
+# print( "harry"[1])
+# print( "harry"[2])
+# print( "harry"[3])
+# print( "harry"[4])
+
+# print("lets print a for loop \n")
+# for char in st:
+#     print(char)
+
+james=("harry, barry, larry")
+print(james[0:5])
+print(len(james))
+print(james[1:3])
+print(james[:19])
+print(james[0:-7])
+nm="harry"
+print(nm[-4:-2])
