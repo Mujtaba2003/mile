376d71389a24da25a2ad5b3291500897178c67b6
Mujtaba Asim <mujtabaasim701@gmail.com>
2024-08-11 12:57:48 +0100

    practice

--- If-Else-Learning2.py
+++ If-Else-Learning2.py
@@ -1,4 +1,4 @@
-#Program about if else using Strings
+# Program about if else using Strings
 # a = input("Enter a name") # koi bhi name enter kerna ha
 #
 # if a.__contains__("a"):
@@ -9,13 +9,13 @@
 #      print("i dont know")
 
 
-#check kere Kashan print kerde kashan found
-#check kere Mujtaba print kerde mujtaba found
-#kuch bhi nahi ha print i dont know
+# check kere Kashan print kerde kashan found
+# check kere Mujtaba print kerde mujtaba found
+# kuch bhi nahi ha print i dont know
 
-#My name is kashan asim
-#My name is mujtaba asim
-#My name is mujtaba asim and my brother's name is Is kashan asim
+# My name is kashan asim
+# My name is mujtaba asim
+# My name is mujtaba asim and my brother's name is Is kashan asim
 
 # o = input("Enter a name")
 # if o. __contains__("kashan"):
@@ -29,9 +29,9 @@
 
 
 
-# what = '''My name is kashan asim
+# What = '''My name is kashan asim
 # My name is mujtaba asim
-# My name is mujtaba asim and my brother's name is Is kashan asim
+# My name is mujtaba asim and my brother's name is kashan asim
 # and i m trying to print a single string consisting of multiple'''
 # print(what)
 #
@@ -45,7 +45,8 @@
 # # print(st)
 #
 # name = "harry"
-# friend = "humaim"
+# frien
+# d = "humaim"
 # print(name,friend)
 # otherfriend = 'my name is husnain " and i m studing" in third semester'
 # print( "harry"[0])
@@ -58,11 +59,104 @@
 # for char in st:
 #     print(char)
 
-james=("harry, barry, larry")
-print(james[0:5])
-print(len(james))
-print(james[1:3])
-print(james[:19])
-print(james[0:-7])
-nm="harry"
-print(nm[-4:-2])
+# james="""harry, barry, larry, carry, jerry, """
+# print(james[0:5])
+# print(len(james))
+# print(james[1:3])
+# print(james[:19])
+# print(james[0:-7])
+# nm="harry"
+# print(nm[-4:-2])
+# print(james[7:15])
+# print(len(james))
+#
+# print("lets print a for loop \n")
+# for fit in james:
+#     print(fit)
+
+# z = "barry"
+# print(z.upper())
+# print(z)
+# print(z.upper())
+
+
+print("enter a name ")
+m = input()
+print(m)
+
+a = int(input("Enter the value of a = "))
+b = int(input("Enter the value of b = "))
+
+print("which function you want to run")
+w = input()
+
+if w.__contains__("+"):
+    print("a+b =", a+b)
+
+elif w.__contains__("-"):
+    print("a-b =", a-b)
+
+elif w.__contains__("*"):
+    print("a*b =", a*b)
+
+elif w.__contains__("/"):
+    print("a/b =", a/b)
+
+else:
+    print("nothing")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print("a+b =", a+b)
+# print("a-b =", a-b)
+# print("a*b =", a*b)
+# # print("a percentile b =", a % b)
